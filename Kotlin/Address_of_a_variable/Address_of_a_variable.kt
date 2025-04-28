10e367617f9100971f11f38b1785360e6730146b
Raafat Abualazm <raafat.abualazm96@gmail.com>
2025-04-28 07:52:15 -0500

    Added More Examples

---
+++ Kotlin/Address_of_a_variable/Address_of_a_variable.kt
@@ -0,0 +1,8 @@
+import kotlinx.cinterop.*
+
+fun main(args: Array<String>) {
+    val intVar = nativeHeap.alloc<IntVar>()
+    intVar.value = 42
+    with(intVar) { println("Value is $value, address is $rawPtr") }
+    nativeHeap.free(intVar)
+}
